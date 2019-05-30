d6b083ab2f4c6a60cdb591cbb7ead7b9229645f2
michal bojanowicz <boramaapps@gmail.com>
2019-05-31 01:04:27 +0200

    first commit

---
+++ UICollectionViewInUIView/CustomCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCell.swift
+//  UICollectionViewInUIView
+//
+//  Created by michal on 31/05/2019.
+//  Copyright © 2019 borama. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UICollectionViewInUIView/CustomView.swift
@@ -0,0 +1,56 @@
+//
+//  CustomView.swift
+//  UICollectionViewInUIView
+//
+//  Created by michal on 31/05/2019.
+//  Copyright © 2019 borama. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: type(of: self))
+        bundle.loadNibNamed("CustomView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.backgroundColor = .red
+        initCollectionView()
+    }
+    
+    private func initCollectionView() {
+        let nib = UINib(nibName: "CustomCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "CustomCell")
+        collectionView.dataSource = self
+    }
+}
+
+extension CustomView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as? CustomCell else {
+            fatalError("can't dequeue CustomCell")
+        }
+        cell.label.text = "\(indexPath.item)"
+        return cell
+    }
+}
