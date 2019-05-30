ee634b8e441f2ddee83febd4a29f2d6eb62205a2
michal bojanowicz <boramaapps@gmail.com>
2019-05-31 00:12:07 +0200

    Initial Commit

---
+++ UICollectionViewInUIView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewInUIView
+//
+//  Created by michal on 31/05/2019.
+//  Copyright © 2019 borama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
